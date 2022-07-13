674278df15ae25569525800ad939f271f626a395
Kleber <kleberst88@gmail.com>
2022-07-13 16:20:04 -0300

    atualizando index

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const getBtn = document.getElementById('bt-enviar');
+const getInputEmail = document.getElementById('')
 
 function clickbtn() {
   getBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log('oi');
+    console.log();
   });
 }
 clickbtn();
