d1ba99b489d8211a9ca41d5da399ddfdf645c491
Kleber <kleberst88@gmail.com>
2022-07-13 16:23:26 -0300

    Validacao do botao Requisito 3

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const getBtn = document.getElementById('bt-enviar');
-const getInputEmail = document.getElementById('')
+const getInputEmail = document.getElementById('email');
+const getInputSenha = document.getElementById('senha');
 
 function clickbtn() {
   getBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log();
+    if (getInputEmail.value === 'tryber@teste.com' && getInputSenha.value === '123456') {
+      alert('Olá, Tryber!');
+    }alert('Email ou senha inválidos.');
   });
 }
 clickbtn();
